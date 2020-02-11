d838c977a59a4f4fd2a6797a9fc111f2438f0d2b
Asia Valentine <asiavalentine22@gmail.com>
2020-02-11 18:01:52 -0500

    Updates

---
+++ precache-manifest.2dc75a1846c03d28bce28c20e65c2509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3673a702538e7bd997f7a632ea611a02",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "72923876f8857bcf7d95",
+    "url": "/currency-converter/static/css/main.9325a7bb.chunk.css"
+  },
+  {
+    "revision": "563413847e8124e902b3",
+    "url": "/currency-converter/static/js/2.8f7c4e5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/currency-converter/static/js/2.8f7c4e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72923876f8857bcf7d95",
+    "url": "/currency-converter/static/js/main.439fa272.chunk.js"
+  },
+  {
+    "revision": "35bda9f6ce4577ccfe4b",
+    "url": "/currency-converter/static/js/runtime-main.f9481c42.js"
+  }
+]);
